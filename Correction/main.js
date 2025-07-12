ac93776af15ac4d9859de27013f55f1a8192a5e3
MathGg1234 <90738758+MathGg1234@users.noreply.github.com>
2025-07-12 20:10:31 +0200

    Update main.js

--- Correction/main.js
+++ Correction/main.js
@@ -1,1 +1,49 @@
+const divs = document.querySelectorAll('div');
+const WIKIPEDIA_FR_BASE_URL = 'https://fr.wikipedia.org/wiki/';
+const COLORS = {
+    RED: 'red',
+    GREEN: 'green',
+    BLUE: 'blue'
+};
 
+const changerCouleurFond = (element, color) => {
+    element.style.backgroundColor = color;
+};
+
+
+const obtenirCouleurFond = (element) => {
+    return getComputedStyle(element).getPropertyValue('background-color');
+};
+
+const extraireValeursRGB = (colorString) => {
+    const valeursRGB = colorString.match(/\d+/g);
+    return {
+        red: parseInt(valeursRGB[0]),
+        green: parseInt(valeursRGB[1]),
+        blue: parseInt(valeursRGB[2])
+    };
+};
+
+const determinerCouleurDominante = ({ red, green, blue }) => {
+    if (red > green && red > blue) {
+        return COLORS.RED;
+    }
+    if (green > red && green > blue) {
+        return COLORS.GREEN;
+    }
+    return COLORS.BLUE;
+};
+
+
+divs.forEach(div => {
+    div.addEventListener('click', () => {
+
+        const couleurActuelle  = obtenirCouleurFond(div);
+        const valeursRGB  = extraireValeursRGB(couleurActuelle);
+        const couleurDominante  = determinerCouleurDominante(valeursRGB);
+
+        changerCouleurFond(div, couleurDominante);
+        window.open(`${WIKIPEDIA_FR_BASE_URL}${couleurDominante}`);
+
+    });
+});
